5664a1c9e9e3a6f4b7ee6a316f96eb505b83b8f9
Gilberto Villa <ingilniero@gmail.com>
2013-12-04 22:34:07 -0600

    Refactor Liker object methods to have a single responsability

--- js/lib/single_responsability.js
+++ js/lib/single_responsability.js
@@ -1,9 +1,38 @@
 var Liker = function() {
-  $('#like').on('click', function(e) {
-    e.preventDefault();
-
-    $(this).html(parseInt($(this).html()) + 1);
-
-    $('#like-count').append('<li>' + new Date().toString() + '</li>');
-  });
+  this.like = 1;
+  this.bind();
 };
+
+Liker.prototype = {
+  updateLike : function() {
+    this.like++;
+    this.publish('liked');
+
+  },
+
+  render : function() {
+    this.updateButton();
+    this.logEntry();
+  },
+
+  updateButton : function() {
+    $('#like').html(this.like);
+  },
+
+  logEntry : function() {
+    $('#like-count').append('<li>' + new Date().toString() + '</li>');
+  },
+
+  bind : function() {
+    var that = this;
+    $('#like').on('click', function(e) {
+      e.preventDefault();
+
+      that.updateLike();
+    });
+
+    this.subscribe('liked', function() { this.render(); });
+  }
+};
+
+Liker.mixin(PubSub);
