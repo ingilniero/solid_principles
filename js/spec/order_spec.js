eb18c8d652c36b56aff8b5a3e7c14e8ee3fe6587
Gilberto Villa <ingilniero@gmail.com>
2013-12-07 14:09:39 -0600

    Demostrate Liskov substitution Principle

--- js/spec/order_spec.js
+++ js/spec/order_spec.js
@@ -4,11 +4,11 @@ describe('Book rental', function() {
     var book = new Book('Lord of the Rings');
 
     it('belongs to user', function() {
-      expect(new Order(user).user).toBe(user);
+      expect(new FlexibleOrder(user).user).toBe(user);
     });
 
     it('can have books', function() {
-      var order = new Order(user);
+      var order = new FlexibleOrder(user);
       order.addBook(book);
       expect(order.items[0].book).toBe(book);
     });
@@ -17,7 +17,7 @@ describe('Book rental', function() {
       var second_book = new Book('Harry Potter', 3);
 
       it('sets the appropiate day limit', function() {
-        var order = new Order(user);
+        var order = new FlexibleOrder(user);
         order.addBook(book);
         order.addBook(second_book);
 
