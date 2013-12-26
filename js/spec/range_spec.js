ce014bc2b2f37efe782d6a7296ed94e4eb666285
Gilberto Villa <ingilniero@gmail.com>
2013-12-26 15:53:56 -0600

    etup full example and add range specs

---
+++ js/spec/range_spec.js
@@ -0,0 +1,14 @@
+describe('Range', function() {
+  it('has a start', function() {
+    expect(new Range().get('start')).toEqual(1);
+  });
+
+  it('has an end', function() {
+    expect(new Range().get('end')).toEqual(9999);
+  });
+
+  it('has a list of hour ranges that starts with one range', function() {
+    expect(new Range().get('hour_ranges').length).toEqual(1);
+  });
+
+});
